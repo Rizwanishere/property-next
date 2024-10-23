44d28cc2c7c0a029d923c1385c5aaadb4fdda00e
Rizwanishere <rizwantopper@gmail.com>
2024-10-24 01:32:54 +0530

    Homepage: Hero, Dynamic InfoBoxes, Footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Homepage</h1>
-      <Link href="/properties">Go to Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
   );
 };
 export default HomePage;
