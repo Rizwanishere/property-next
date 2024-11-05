898229beacfadc76156c1346357f071abc97ef22
Rizwanishere <rizwantopper@gmail.com>
2024-11-06 00:05:37 +0530

    Edit Property Form

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -219,7 +219,7 @@ const Navbar = () => {
                       id="user-menu-item-2"
                       onClick={() => {
                         setIsProfileMenuOpen(false);
-                        signOut();
+                        signOut({ callbackUrl: "/" });
                       }}
                     >
                       Sign Out
