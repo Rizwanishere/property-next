7d7f95392bcf55ef455d2ac4820f022ec9be7bc7
Rizwanishere <rizwantopper@gmail.com>
2024-11-06 22:50:11 +0530

    Bookmark button and Action

---
+++ app/actions/bookmarkProperty.js
@@ -0,0 +1,43 @@
+"use server";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+
+async function bookmarkProperty(propertyId) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    return { error: "User ID is required" };
+  }
+
+  const { userId } = sessionUser;
+
+  // Find user in db
+  const user = await User.findById(userId);
+
+  // Check if the property is bookmarked
+  let isBookmarked = user.bookmarks.includes(propertyId);
+
+  let message;
+
+  if (isBookmarked) {
+    // If already bookmarked, remove it
+    user.bookmarks.pull(propertyId);
+    message = "Bookmark removed successfully";
+    isBookmarked = false;
+  } else {
+    // If not bookmarked, add it
+    user.bookmarks.push(propertyId);
+    message = "Bookmark added successfully";
+    isBookmarked = true;
+  }
+
+  await user.save();
+  revalidatePath("/properties/saved", "page");
+
+  return { message, isBookmarked };
+}
+
+export default bookmarkProperty;
