0cf444a0890eadbc79e2963b7b70555d8cbf73c4
Rizwanishere <rizwantopper@gmail.com>
2024-11-13 00:16:07 +0530

    Check bookmark status
    Bookmark button persistance even after page reload

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -4,21 +4,27 @@ import { useSession } from "next-auth/react";
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
 import { toast } from "react-toastify";
 import { FaBookmark } from "react-icons/fa";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
-  const userId = session.user?.id;
+  const userId = session?.user?.id;
 
   const [isBookmarked, setIsBookmarked] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!userId) {
       setLoading(false);
       return;
     }
-    setLoading(false);
-  }, [property._id, userId]);
+
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) toast.error(res.error);
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
 
   const handleClick = async () => {
     if (!userId) {
@@ -26,24 +32,24 @@ const BookmarkButton = ({ property }) => {
       return;
     }
 
-    try {
-      const res = await bookmarkProperty(property._id);
-      if (res.error) {
-        toast.error(res.error);
-      } else {
-        toast.success(res.message);
-      }
-    } catch (error) {
-      console.error("Error bookmarking property:", error);
-      toast.error("Failed to bookmark property");
-    } finally {
-      setLoading(false);
-    }
+    bookmarkProperty(property._id).then((res) => {
+      if (res.error) toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
+      toast.success(res.message);
+    });
   };
 
   if (loading) return <p className="text-center">Loading...</p>;
 
-  return (
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" />
+      Remove Bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
